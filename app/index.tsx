8966e745adb79ae62d4e117e261a57fb3ffb3f63
Carl <10485521@ucn.dk>
2025-09-04 13:47:46 +0200

    ToDoApp Setup

---
+++ app/index.tsx
@@ -0,0 +1,59 @@
+import { Ionicons } from '@expo/vector-icons';
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  const todoData = [
+    {
+      id: 1,
+      title: "Todo 1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      isDone: false,
+    },
+    {
+      id: 4,
+      title: "Todo 4",
+      isDone: false,
+    },
+    {
+      id: 5,
+      title: "Todo 5",
+      isDone: false,
+    },
+    {
+      id: 6,
+      title: "Todo 6",
+      isDone: false,
+    },
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Ionicons name="menu" size={24} color={'#333'} />
+      </View>
+      <FlatList data={todoData} keyExtractor={(item) => item.id.toString()} renderItem={({item}) => (
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      )} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+})
